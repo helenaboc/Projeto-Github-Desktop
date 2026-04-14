c27ce5964fea2f0c0e6b2134e94263b79561bc01
helenaboc <helenabonesi@gmail.com>
2026-04-14 11:22:57 -0300

    novas tarefas
    novas tarefas

---
+++ Projeto-github/ex002/script.js
@@ -0,0 +1,36 @@
+function pedirnome(){
+    let nome
+    let regexNome = /^[A-Za-zÀ-ÿ\s`-]+$/
+    while (true){
+        nome = prompt("Digite o seu nome completo:")
+        if(nome === null){
+            alert("O nome não pode ficar vazio.")
+            continue;
+        }
+        if(regexNome.teste(nome)){
+            alert ("Digite apenas letras e espaços no nome.");
+            continue;
+        }
+        let partesNome = nome.split(/\s+/);
+        if (partesNome.length < 2){
+            alert("Digite o nome completo, com nome e sobrenome.")
+        continue/
+        }
+        if (nome.length < 3) {
+            alert("Digite um nome válido")
+            continue;
+                        
+            function IniciarCadastro(){
+                let nome = pedirNome();
+                if (nome === null){
+                    document.getElementById(resultado).innerHTML =
+                    "<H2>Cadastro cancelado</h2>" +
+                    "<p>O usuário cancelou o preenchimento</p>"
+                    return;
+                }
+            document.getElementById("resultado").innerHTML =
+            "<h2>Resultado</h2>" +
+            "<p><strong>Nome Completo:<strong></p>"
+            }
+            
+        }
---
+++ Projeto-github/ex003/script.js
@@ -0,0 +1,38 @@
+function obterGenero() { 
+    let genero;
+    
+    while (true) {
+        genero = prompt("Digite seu gênero: masculino, feminino ou outro")
+       
+        if(genero === null) {
+            alert("Cadastro cancelado.")
+            return null;     
+        }
+        genero = genero.trim().toLowerCase();
+
+        if (genero === "") {
+            alert("O campo gênero não pode ficar vazio.")
+            continue;
+        }
+
+        if (genero !== "masculino" && genero !== "feminino" && genero !== "outro") {
+            alert("Digite apenas: masculino, feminino ou outro.")
+            continue;
+        }
+
+        return genero;
+    }
+}
+    
+function iniciarCadastro() {
+    let genero = obterGenero()
+    
+        if (genero === null) {
+        document.getElementById("resultado").innerHTML =
+        "<H2>Cadastro cancelado</h2><p>O usuário cancelou o preenchimento</p>"
+        return;
+        }
+        
+        document.getElementById("resultado").innerHTML =
+        "<h2>Resultado</h2><p><strong>Gênero:</strong>" + genero + "</p>";
+}            
